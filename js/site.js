24653e50561ecc11026b9b166483749d9c89a82b
Roman <61121402+rebrov-rc@users.noreply.github.com>
2020-03-05 23:54:14 +0300

    05.03 23:54

--- js/site.js
+++ js/site.js
@@ -1,9 +1,30 @@
 let a0 = 8;
 let a1 = 20;
 
-let a2 = 9;
+let a2 = 21;
 
+if (a2 > a0) {
+    if (a2 < a1) {
+    console.log('Открыто!');
+
+    }
+
+    console.log('Открыто!');
+
+} else {
+    console.log('Закрыто!');
+}
+
+
+
+
+
+// =====================
 if (a2 > a0 && a2 < a1) {
+    if (a2 < a1) {
+    console.log('Открыто!');
+
+    }
 
     console.log('Открыто!');
 
